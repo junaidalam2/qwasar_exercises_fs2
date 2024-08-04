0fbf4d4113cd96ccbffe4ec91afb4b1b0cc36b89
junaidalam2 <junaid.alam2@gmail.com>
2024-08-04 16:32:04 -0700

    worked on maxlenoc

---
+++ str_maxlenoc.js
@@ -0,0 +1,37 @@
+
+
+param_1 = ["ab", "bac", "abacabccabcb"] 
+param_2 = 3
+
+
+param_1 = ["bonjour", "salut", "bonjour", "bonjour"] 
+param_2 = 4
+
+param_1 = ["xoxAoxo", "xoxAox", "oxAox"] 
+param_2 = 3
+
+function str_maxlenoc(param_1, param_2) {
+
+    param_1.sort((a, b) => { 
+        return a.length = b.length;
+    })
+
+    let first_element = param_1[0];
+    param_1.shift();
+
+    let i = 0;
+    let k = 0;
+
+    for(let i = 0; i < first_element.length; i++) {
+
+        for(let j = 0; j < param_1.length; j++) {
+            
+        }
+        
+
+    }
+
+
+
+
+};
