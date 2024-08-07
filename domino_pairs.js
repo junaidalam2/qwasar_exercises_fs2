875963bede09737e770b1b1be1b330e383be7ac7
junaidalam2 <junaid.alam2@gmail.com>
2024-08-07 13:20:10 -0700

    created possible solution to be tested

--- domino_pairs.js
+++ domino_pairs.js
@@ -6,17 +6,21 @@ dominoes = [[1,2],[2,1],[3,4],[5,6]]
 
 function number_of_equivalent_domino_pairs(dominoes) {
 
-    domino_object = {
-
-    }
+    domino_object = {}
+    let counter = 0;
 
     for(domino of dominoes) {
-
-        
-
-
-
+        domino.sort((a, b) => a - b);
+        if(domino_object[domino]) {
+            counter++;
+        } else {
+            domino_object[domino] = true;
+        }
     }
+
+    return counter;
 
 
 };
+
+console.log(number_of_equivalent_domino_pairs(dominoes));
