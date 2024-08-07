bf44053da8242f046852e6bc6ee69ffa8ca999ec
junaidalam2 <junaid.alam2@gmail.com>
2024-08-07 13:11:49 -0700

    started domino pairs

---
+++ domino_pairs.js
@@ -0,0 +1,22 @@
+
+
+
+
+dominoes = [[1,2],[2,1],[3,4],[5,6]]
+
+function number_of_equivalent_domino_pairs(dominoes) {
+
+    domino_object = {
+
+    }
+
+    for(domino of dominoes) {
+
+        
+
+
+
+    }
+
+
+};
